73b61011bc52cba17dd160ff206ecab0b8b3e3bf
hendrixjoseph <hendixjoseph@aol.com>
2020-08-20 21:59:03 -0400

    couple quick fixes

--- jekyll-tag-page-generator.js
+++ jekyll-tag-page-generator.js
@@ -13,18 +13,18 @@ title: Posts with tag "${tag.name}"
 tag: ${tag.name}
 ---`
 
-exports.writeFile = (destination, tag) => {	
+exports.writeFile = (destination, tag) => {
 	fs.mkdir(destination, { recursive: true }, () => {
 		let content = exports.getFileContents(tag)
 		let filename = tag.slug + '.md'
-		
+
 		fs.writeFile(destination + filename, content, { flag: 'wx' }, err => {
 			if (err) {
 				core.info(filename + ' already exists!')
 			}
 		})
 	})
-	
+
 	return Promise.resolve()
 }
 
@@ -36,7 +36,7 @@ exports.getTags = source => {
     .then(json => json.tags)
 }
 
-exports.deleteUnused = (destination, tags) => {	
+exports.deleteUnused = (destination, tags) => {
 	fs.readdir(destination, (err, files) => {
 		files.forEach(file => {
 			if (tags.some(tag => tag.slug + ".md" !== file)) {
@@ -44,23 +44,23 @@ exports.deleteUnused = (destination, tags) => {
 					  if (err) throw err;
 					  core.info(file + ' was deleted');
 				});
-				
+
 			}
 		})
 	})
-	
+
 	return tags
 }
 
-exports.commit = (destination, token) => {
+const commit = (destination, token) => {
     if (token) {
-	  await exec('git', ['remote', 'set-url', 'origin',
+	  await exec.exec('git', ['remote', 'set-url', 'origin',
 	    `https://${GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`])
 	}
-	
+
 	await exec.exec('git', ['config', '--global', 'user.name', 'jekyll-tag-page-generator'])
-	await exec.exec('git', ['add', './destination/*.md'])
-	await exec.exec('git', ['add', '-u', './destination/*.md'])
+	await exec.exec('git', ['add', destination + '/*.md'])
+	await exec.exec('git', ['add', '-u', destination + '/*'])
 	await exec.exec('git', ['commit', '-m', 'updating tag directory])
 	await exec.exec('git', ['push'])
 }
@@ -70,12 +70,12 @@ exports.main = () => {
   const destination = core.getInput('destination')
   const github_token = core.getInput('gh_token');
   core.setSecret(github_token);
-  
+
   exports.getTags(source)
   		 .then(tags => exports.deleteUnused(destination, tags))
          .then(tags => {
         	 tags.forEach(tag => exports.writeFile(destination, tag))
         	 return tags
          })
-         .then(() => exports.commit(destination, github_token))
+         .then(() => commit(destination, github_token))
 }
