6d83c81ff8b274e101e1e0610a30f8c5187205e3
Kyle Zhou <51883176+Kyle-Zhou@users.noreply.github.com>
2023-10-06 16:27:09 -0400

    remove push

--- jekyll-tag-page-generator.js
+++ jekyll-tag-page-generator.js
@@ -92,9 +92,9 @@ const commit = async (destination, token) => {
   await exec.exec(cmd, "", options);
   if (myOutput.trim() !== "") {  // If there are staged changes
     await exec.exec('git', ['commit', '-m', 'updating tag directory']);
-		const branchName = await getBranchName();
-    // await exec.exec('git', ['push', 'origin', branchName]);
-		await exec.exec('git', ['push']);
+		// const branchName = await getBranchName();
+    // // await exec.exec('git', ['push', 'origin', branchName]);
+		// await exec.exec('git', ['push']);
 
   } else {
     console.log('No changes staged for commit.');
